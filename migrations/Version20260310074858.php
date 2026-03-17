406027d387a659b290396e0039e79522456be96d
Patrick <bevis.patrick@gmail.com>
2026-03-17 15:44:30 +0100

    feat: API params

--- migrations/Version20260310074858.php
+++ migrations/Version20260310074858.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260309132534 extends AbstractMigration
+final class Version20260310074858 extends AbstractMigration
 {
     public function getDescription(): string
     {
